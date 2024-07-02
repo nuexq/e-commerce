1fb2bee8809dad66de7c013e95776e250e4238bd
No0ne003 <hi.yassir.op@gmail.com>
2024-07-02 13:00:36 +0100

    style: flex-1 on content of admin pages

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -8,7 +8,7 @@ export default function AdminLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Nav>
         <nav className="flex items-center space-x-4 lg:space-x-6">
           <NavLink href="/admin">Dashboard</NavLink>
@@ -17,7 +17,7 @@ export default function AdminLayout({
           <NavLink href="/admin/orders">Sales</NavLink>
         </nav>
       </Nav>
-      <div className="container my-6">{children}</div>
-    </>
+      <div className="container my-6 flex-1">{children}</div>
+    </div>
   );
 }
--- src/app/admin/loading.tsx
+++ src/app/admin/loading.tsx
@@ -2,7 +2,7 @@ import { Loader2 } from "lucide-react";
 
 export default function AdminLoading() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
+    <div className="flex justify-center">
       <Loader2 className="size-24 animate-spin" />
     </div>
   );
