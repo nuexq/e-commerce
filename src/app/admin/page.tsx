6e9696b1d3fe18bc103cc8ea45cb40361835a4a1
No0ne003 <hi.yassir.op@gmail.com>
2024-07-02 11:53:56 +0100

    feat: dashboard cards component

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,36 @@
-export default function AdminDashboard() {
-  return <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
-  </div>
+export default function AdminDashboard() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <DashboardCard title="Sales" subtitle="Test" body="body" />
+      <DashboardCard title="Sales" subtitle="Test" body="body" />
+      <DashboardCard title="Sales" subtitle="Test" body="body" />
+    </div>
+  );
+}
+
+type DashboardCardProps = {
+  title: string;
+  subtitle: string;
+  body: string;
+};
+
+function DashboardCard({ title, subtitle, body }: DashboardCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+
+        <CardDescription>{subtitle}</CardDescription>
+      </CardHeader>
+      <CardContent>{body}</CardContent>
+    </Card>
+  );
 }
