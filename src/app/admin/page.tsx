47bcec86ac9931f4e7513b1e5d685b4fabfae6f4
No0ne003 <hi.yassir.op@gmail.com>
2024-07-02 12:07:11 +0100

    chore: formation utils and use it on salles data

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -6,6 +6,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import db from "@/db/db";
+import { formatCurrency, formatNumber } from "@/lib/formations";
 
 async function getSalesData() {
   const data = await db.order.aggregate({
@@ -23,7 +24,11 @@ export default async function AdminDashboard() {
   const salesData = await getSalesData();
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      <DashboardCard title="Sales" subtitle={salesData.numberOfSales} body={salesData.amount} />
+      <DashboardCard
+        title="Sales"
+        subtitle={formatNumber(salesData.numberOfSales)}
+        body={formatCurrency(salesData.amount)}
+      />
     </div>
   );
 }
