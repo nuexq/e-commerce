cd552ae7ea642ed28579fa2e9e68867a488a2ab2
No0ne003 <hi.yassir.op@gmail.com>
2024-07-02 12:10:58 +0100

    chore:

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -26,7 +26,7 @@ export default async function AdminDashboard() {
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       <DashboardCard
         title="Sales"
-        subtitle={formatNumber(salesData.numberOfSales)}
+        subtitle={`${formatNumber(salesData.numberOfSales)} Orders`}
         body={formatCurrency(salesData.amount)}
       />
     </div>
