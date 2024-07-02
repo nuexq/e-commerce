473db447dc5c85da4665af64627e34b28cfc50a1
No0ne003 <hi.yassir.op@gmail.com>
2024-07-02 12:02:29 +0100

    chore: getSalesData function

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -5,6 +5,19 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import db from "@/db/db";
+
+async function getSalesData() {
+  const data = await db.order.aggregate({
+    _sum: { pricePaidInCents: true },
+    _count: true,
+  });
+
+  return {
+    amount: (data._sum.pricePaidInCents || 0) / 100,
+    numberOfSales: data._count
+  }
+}
 
 export default function AdminDashboard() {
   return (
