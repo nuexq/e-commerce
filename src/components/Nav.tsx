f8f854ff0e7c7fb60f45795873926b6cae0eaf58
No0ne003 <hi.yassir.op@gmail.com>
2024-07-02 11:19:21 +0100

    feat: nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,3 +1,9 @@
-export function Nav() {
-  return <p>nav</p>
+import { ReactNode } from "react";
+
+export function Nav({ children }: { children: ReactNode }) {
+  return (
+    <nav className="bg-primary text-primary-foreground flex justify-center px-4">
+      {children}
+    </nav>
+  );
 }
