0a07037f111a718d7c7f70566629c42af5df9969
No0ne003 <hi.yassir.op@gmail.com>
2024-07-02 12:04:21 +0100

    chore: add sales data to card

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -15,16 +15,15 @@ async function getSalesData() {
 
   return {
     amount: (data._sum.pricePaidInCents || 0) / 100,
-    numberOfSales: data._count
-  }
+    numberOfSales: data._count,
+  };
 }
 
-export default function AdminDashboard() {
+export default async function AdminDashboard() {
+  const salesData = await getSalesData();
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      <DashboardCard title="Sales" subtitle="Test" body="body" />
-      <DashboardCard title="Sales" subtitle="Test" body="body" />
-      <DashboardCard title="Sales" subtitle="Test" body="body" />
+      <DashboardCard title="Sales" subtitle={salesData.numberOfSales} body={salesData.amount} />
     </div>
   );
 }
