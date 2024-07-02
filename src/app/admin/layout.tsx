af150b5f84282ac9865105166891760f0cb060ce
No0ne003 <hi.yassir.op@gmail.com>
2024-07-02 11:28:26 +0100

    feat: add links

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,4 +1,4 @@
-import { Nav } from "@/components/Nav";
+import { Nav, NavLink } from "@/components/Nav";
 
 export default function AdminLayout({
   children,
@@ -7,7 +7,12 @@ export default function AdminLayout({
 }>) {
   return (
     <>
-      <Nav></Nav>
+      <Nav>
+        <NavLink href="/admin">Dashboard</NavLink>
+        <NavLink href="/admin/Products">Products</NavLink>
+        <NavLink href="/admin/users">Users</NavLink>
+        <NavLink href="/admin/orders">Sales</NavLink>
+      </Nav>
       <div className="container my-6">{children}</div>
     </>
   );
