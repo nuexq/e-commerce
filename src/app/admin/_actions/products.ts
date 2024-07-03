fa2f1d2a88b7746fd9d930d860073eedccef589e
No0ne003 <hi.yassir.op@gmail.com>
2024-07-03 12:34:14 +0100

    feat: create product go to database and add imagePath to prisma database

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -1,9 +1,12 @@
 "use server";
 
+import db from "@/db/db";
 import { z } from "zod";
+import fs from "fs/promises";
+import { redirect } from "next/navigation";
 
 const fileSchema = z.instanceof(File, { message: "Required" });
-const imageShcema = fileSchema.refine(
+const imageSchema = fileSchema.refine(
   (file) => file.size === 0 || file.type.startsWith("image/"),
 );
 
@@ -12,7 +15,7 @@ const addSchema = z.object({
   priceInCents: z.coerce.number().int().min(1),
   description: z.string().min(1),
   file: fileSchema.refine((file) => file.size > 0, "Required"),
-  image: imageShcema.refine((file) => file.size > 0, "Required"),
+  image: imageSchema.refine((file) => file.size > 0, "Required"),
 });
 
 export async function addProduct(formData: FormData) {
@@ -22,4 +25,28 @@ export async function addProduct(formData: FormData) {
   }
 
   const data = result.data;
+
+  await fs.mkdir("products", { recursive: true });
+  const filePath = `products/${crypto.randomUUID()}-${data.file.name}`;
+  await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()));
+
+  await fs.mkdir("public/products", { recursive: true });
+  const imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`;
+  await fs.writeFile(
+    `public${imagePath}`,
+    Buffer.from(await data.image.arrayBuffer()),
+  );
+
+  await db.product.create({
+    data: {
+      isAvailableForPurchase: false,
+      name: data.name,
+      description: data.description,
+      priceInCents: data.priceInCents,
+      filePath,
+      imagePath,
+    },
+  });
+
+  redirect("/admin/products");
 }
