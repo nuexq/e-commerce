398054fbe60f444c40a1e604bc2fd8e6dca65e32
No0ne003 <hi.yassir.op@gmail.com>
2024-07-03 12:13:46 +0100

    feat: product form validation

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -1,5 +1,20 @@
 "use server";
 
+import { z } from "zod";
+
+const fileSchema = z.instanceof(File, { message: "Required" });
+const imageShcema = fileSchema.refine(
+  (file) => file.size === 0 || file.type.startsWith("image/"),
+);
+
+const addSchema = z.object({
+  name: z.string().min(1),
+  priceInCents: z.coerce.number().int().min(1),
+  description: z.string().min(1),
+  file: fileSchema.refine((file) => file.size > 0, "Required"),
+  image: imageShcema.refine((file) => file.size > 0, "Required"),
+});
+
 export async function addProduct(formData: FormData) {
-  console.log(formData)
+  console.log(formData);
 }
