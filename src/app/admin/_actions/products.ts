0446a80e5ac6f583612427346a5b26af56bfa937
No0ne003 <hi.yassir.op@gmail.com>
2024-07-04 13:35:06 +0100

    activeToggleDropdown menu function and toggleProductAvailability action

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -50,3 +50,10 @@ export async function addProduct(prevState: unknown, formData: FormData) {
 
   redirect("/admin/products");
 }
+
+export async function toggleProductAvailability(
+  id: string,
+  isAvailableForPurchase: boolean,
+) {
+  await db.product.update({ where: { id }, data: { isAvailableForPurchase } });
+}
