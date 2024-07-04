9e12c942e6f1a9dd7597a9c6a15e68dda04009ef
No0ne003 <hi.yassir.op@gmail.com>
2024-07-04 13:38:58 +0100

    refactor: formating

--- src/app/admin/products/_components/ProductActions.tsx
+++ src/app/admin/products/_components/ProductActions.tsx
@@ -1,58 +1,58 @@
-"use client"
+"use client";
 
-import { DropdownMenuItem } from "@/components/ui/dropdown-menu"
-import { useTransition } from "react"
+import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
+import { useTransition } from "react";
 import {
   deleteProduct,
   toggleProductAvailability,
-} from "../../_actions/products"
-import { useRouter } from "next/navigation"
+} from "../../_actions/products";
+import { useRouter } from "next/navigation";
 
 export function ActiveToggleDropdownItem({
   id,
   isAvailableForPurchase,
 }: {
-  id: string
-  isAvailableForPurchase: boolean
+  id: string;
+  isAvailableForPurchase: boolean;
 }) {
-  const [isPending, startTransition] = useTransition()
-  const router = useRouter()
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
   return (
     <DropdownMenuItem
       disabled={isPending}
       onClick={() => {
         startTransition(async () => {
-          await toggleProductAvailability(id, !isAvailableForPurchase)
-          router.refresh()
-        })
+          await toggleProductAvailability(id, !isAvailableForPurchase);
+          router.refresh();
+        });
       }}
     >
       {isAvailableForPurchase ? "Deactivate" : "Activate"}
     </DropdownMenuItem>
-  )
+  );
 }
 
 export function DeleteDropdownItem({
   id,
   disabled,
 }: {
-  id: string
-  disabled: boolean
+  id: string;
+  disabled: boolean;
 }) {
-  const [isPending, startTransition] = useTransition()
-  const router = useRouter()
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
   return (
     <DropdownMenuItem
       variant="destructive"
       disabled={disabled || isPending}
       onClick={() => {
         startTransition(async () => {
-          await deleteProduct(id)
-          router.refresh()
-        })
+          await deleteProduct(id);
+          router.refresh();
+        });
       }}
     >
       Delete
     </DropdownMenuItem>
-  )
+  );
 }
