b2b98aebf0e9425e33fece5b87060d90317d1dd6
No0ne003 <hi.yassir.op@gmail.com>
2024-07-08 13:36:35 +0100

    chore: getmostPupular products function

--- src/app/(customerFacing)/page.tsx
+++ src/app/(customerFacing)/page.tsx
@@ -1,3 +1,13 @@
+import db from "@/db/db";
+
+function getMostPopularProducts() {
+  return db.product.findMany({
+    where: { isAvailableForPurchase: true },
+    orderBy: { orders: { _count: "desc" } },
+    take: 6
+  });
+}
+
 export default function HomePage() {
-  return <h1>hi</h1>
+  return <h1>hi</h1>;
 }
