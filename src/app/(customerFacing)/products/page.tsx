1f45fa8c0430cdec7efec0bc3fc4c7218f5d87a7
No0ne003 <hi.yassir.op@gmail.com>
2024-07-09 11:37:19 +0100

    feat: simple products page

---
+++ src/app/(customerFacing)/products/page.tsx
@@ -0,0 +1,49 @@
+import { ProductCard } from "@/components/ProductCard";
+import db from "@/db/db";
+import { Product } from "@prisma/client";
+import { Suspense } from "react";
+
+function getAllProducts() {
+  return db.product.findMany({
+    where: { isAvailableForPurchase: true },
+    orderBy: { orders: { _count: "desc" } },
+  });
+}
+
+export default function ProductPage() {
+  return (
+    <main>
+      <ProductGrid productsFetcher={getAllProducts} />
+    </main>
+  );
+}
+
+function ProductGrid({
+  productsFetcher,
+}: {
+  productsFetcher: () => Promise<Product[]>;
+}) {
+  return (
+    <div className="space-y-8">
+      <div className="flex flex-col gap-2">
+        <h2 className="text-3xl font-bold">Products</h2>
+        <p className="text-muted-foreground">explore our products</p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <Suspense>
+          <ProductSuspense productsFetcher={productsFetcher} />
+        </Suspense>
+      </div>
+    </div>
+  );
+}
+
+async function ProductSuspense({
+  productsFetcher,
+}: {
+  productsFetcher: () => Promise<Product[]>;
+}) {
+  return (await productsFetcher()).map((product) => (
+    <ProductCard key={product.id} {...product} />
+  ));
+}
