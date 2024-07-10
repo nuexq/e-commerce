b3fbc8eba425647f7cafa3406b45411acdf8c93e
No0ne003 <hi.yassir.op@gmail.com>
2024-07-10 11:49:28 +0100

    chore: making stripe object and installing stripe instead of strip

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,5 +1,8 @@
 import db from "@/db/db";
 import { notFound } from "next/navigation";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function PurchasePage({
   params: { id },
