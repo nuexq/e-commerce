de85d9669a0603674f88d6b699a57355a612af0a
No0ne003 <hi.yassir.op@gmail.com>
2024-07-10 12:26:52 +0100

    feat: payment form

--- src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -1,8 +1,88 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { formatCurrency } from "@/lib/formations";
+import {
+  Elements,
+  PaymentElement,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import Image from "next/image";
+
 type CheckoutFormProps = {
-  product: {}
-  clientSecret: string
-}
+  product: {
+    imagePath: string;
+    name: string;
+    priceInCents: number;
+    description: string;
+  };
+  clientSecret: string;
+};
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string,
+);
 
 export function CheckoutForm({ product, clientSecret }: CheckoutFormProps) {
-  return <h1>form</h1>
+  return (
+    <>
+      <div className="max-w-5xl w-full mx-auto space-y-8">
+        <div className="flex gap-4 items-center">
+          <div className="aspect-video flex-shrink-0 w-1/3 relative">
+            <Image src={product.imagePath} fill alt={product.name} />
+          </div>
+          <div>
+            <div className="text-lg">
+              {formatCurrency(product.priceInCents / 100)}
+            </div>
+            <h1 className="text-2xl font-bold">{product.name}</h1>
+            <div className="line-clamp-3 text-muted-foreground">
+              {product.description}
+            </div>
+          </div>
+        </div>
+        <Elements options={{ clientSecret }} stripe={stripePromise}>
+          <Form priceInCents={product.priceInCents} />
+        </Elements>
+      </div>
+    </>
+  );
+}
+
+function Form({ priceInCents }: { priceInCents: number }) {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  return (
+    <form>
+      <Card>
+        <CardHeader>
+          <CardTitle>Checkout</CardTitle>
+          <CardDescription className="text-destructive">Error</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <PaymentElement />
+        </CardContent>
+        <CardFooter>
+          <Button
+            className="w-full"
+            size="lg"
+            disabled={stripe == null || elements == null}
+          >
+            Purchase - {formatCurrency(priceInCents / 100)}
+          </Button>
+        </CardFooter>
+      </Card>
+    </form>
+  );
 }
