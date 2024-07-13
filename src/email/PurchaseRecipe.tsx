c203240d9e1d432dad6b717f64699d367ffdb733
No0ne003 <hi.yassir.op@gmail.com>
2024-07-13 13:55:43 +0100

    finsishin PurchaseReceipeEmail component

--- src/email/PurchaseRecipe.tsx
+++ src/email/PurchaseRecipe.tsx
@@ -1,5 +1,39 @@
-import { Html } from "react-email";
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Html,
+  Preview,
+  Tailwind,
+} from "@react-email/components";
+import { OrderInformation } from "./components/OrderInformation";
 
-export default function PurchaseReceiptEmail() {
-  return <Html></Html>;
+type PurchaseReceipEmailProps = {
+  product: {
+    name: string;
+  };
+};
+
+PurchaseReceiptEmail.PreviewProps = {
+  product: { name: "Product name" },
+} satisfies PurchaseReceipEmailProps;
+
+export default function PurchaseReceiptEmail({
+  product,
+}: PurchaseReceipEmailProps) {
+  return (
+    <Html>
+      <Preview>Download {product.name} and view receipt</Preview>
+      <Tailwind>
+        <Head />
+        <Body className="font-sans bg-white">
+          <Container className="max-w-xl">
+            <Heading>Purchase Receipt</Heading>
+            <OrderInformation />
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
 }
