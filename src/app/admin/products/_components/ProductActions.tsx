f9c1c87a82624ee3222c898421204604ac45de24
No0ne003 <hi.yassir.op@gmail.com>
2024-07-14 12:12:09 +0100

    style: adding icon to productActions dropdownMenuItem

--- src/app/admin/products/_components/ProductActions.tsx
+++ src/app/admin/products/_components/ProductActions.tsx
@@ -1,58 +1,69 @@
-"use client"
+"use client";
 
-import { DropdownMenuItem } from "@/components/ui/dropdown-menu"
-import { useTransition } from "react"
+import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
+import { useTransition } from "react";
 import {
   deleteProduct,
   toggleProductAvailability,
-} from "../../_actions/products"
-import { useRouter } from "next/navigation"
+} from "../../_actions/products";
+import { useRouter } from "next/navigation";
+import { CircleCheck, CircleOff } from "lucide-react";
 
 export function ActiveToggleDropdownItem({
   id,
   isAvailableForPurchase,
 }: {
-  id: string
-  isAvailableForPurchase: boolean
+  id: string;
+  isAvailableForPurchase: boolean;
 }) {
-  const [isPending, startTransition] = useTransition()
-  const router = useRouter()
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
   return (
     <DropdownMenuItem
       disabled={isPending}
       onClick={() => {
         startTransition(async () => {
-          await toggleProductAvailability(id, !isAvailableForPurchase)
-          router.refresh()
-        })
+          await toggleProductAvailability(id, !isAvailableForPurchase);
+          router.refresh();
+        });
       }}
     >
-      {isAvailableForPurchase ? "Deactivate" : "Activate"}
+      {isAvailableForPurchase ? (
+        <div className="flex gap-2 items-center justify-start">
+          <CircleOff size={14} />
+          <span>Deactivate</span>
+        </div>
+      ) : (
+        <div className="flex gap-2 items-center justify-start">
+          <CircleCheck size={16} />
+          <span>Activate</span>
+        </div>
+      )}
     </DropdownMenuItem>
-  )
+  );
 }
 
 export function DeleteDropdownItem({
   id,
   disabled,
 }: {
-  id: string
-  disabled: boolean
+  id: string;
+  disabled: boolean;
 }) {
-  const [isPending, startTransition] = useTransition()
-  const router = useRouter()
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
   return (
     <DropdownMenuItem
       variant="destructive"
       disabled={disabled || isPending}
       onClick={() => {
         startTransition(async () => {
-          await deleteProduct(id)
-          router.refresh()
-        })
+          await deleteProduct(id);
+          router.refresh();
+        });
       }}
     >
       Delete
     </DropdownMenuItem>
-  )
+  );
 }
